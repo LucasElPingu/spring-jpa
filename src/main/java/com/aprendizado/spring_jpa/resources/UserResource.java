f25261046a57056f970f75be81dc5cd7d02ac88a
LucasElPingu <lucasfernando.soaresssilva@gmail.com>
2025-03-04 21:16:29 -0300

    Camada de serviço e registro de componentes
    Criação da classe de serviço para o User no pacote services para não sobrecarregar o rest com regras de negócios como o findAll(Ele ainda vai repassar a chamada para o repository.findAll) e registrando os componentes como @service ou @entitry ou @component, criando métodos findAll e findById na classe auxiliar serviço.
    OBS:NO SPRING 3 ELE CONSIDERA O "/" OUTRO CAMINHO ELE N]AO IGNORA O BARRA NO FINAL, TEM QQUE USAR EXPRESSÕES REGULARES NO MAPPING PARA ELE CONSIDERAR QUE SEM OU COM BARRA VAI RETORNAR A MESMA COISA EX:@GetMapping({ "", "/"})

--- src/main/java/com/aprendizado/spring_jpa/resources/UserResource.java
+++ src/main/java/com/aprendizado/spring_jpa/resources/UserResource.java
@@ -1,19 +1,46 @@
 package com.aprendizado.spring_jpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aprendizado.spring_jpa.entities.User;
+import com.aprendizado.spring_jpa.services.UserService;
+//OBS SEMPRE LEMBRAR DE FINALIZAR E EXECUTAR DNV NO MEU O RESTART FALHOU UMA VEZ
+//Indica que e um rest controlle no caso o que faz a comunicação entre interface e bd
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u1 = new User(1L, "n", "n", "n", "n"); 
-		return ResponseEntity.ok().body(u1);
+	@Autowired
+	private UserService service;
+	
+	@GetMapping({ "", "/"}) //use expressões regulares de ou um dos dois
+	public ResponseEntity<List<User>> findAll(){
+		List<User>list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	//Responde requisições do tipo get do http
+	/*@GetMapping("/") //O meu spring ta trando o "/" no final como uma rota diferente, testei adicionando a linha
+	//spring.web.mvc.pathmatch.matching-strategy=path_pattern_parser no application.properties, n deu certo
+	public ResponseEntity<List<User>> findAllWhithSlash(){
+		List<User>list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}*/
+	
+	@GetMapping(value = "/{id}") //indica que a requisição vai aceitar um id na url 
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		//o @PathVariable indica que o parametro vai vir pela url
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u);
+	}
+	
 }
---
+++ src/main/java/com/aprendizado/spring_jpa/services/UserService.java
@@ -0,0 +1,36 @@
+package com.aprendizado.spring_jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aprendizado.spring_jpa.entities.User;
+import com.aprendizado.spring_jpa.repositories.UserRepository;
+
+//classe de serviços para o User, não e obrigado da para colocar tudo resource layer, mas ai sobrecarregaria com regras de negocios.
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	//repassara a chamada para o repository.findAll
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		/*O método findById(id) do JpaRepository retorna um Optional<User>.
+		O Optional é uma classe do Java 8 criada para evitar NullPointerException ao lidar com valores que podem ser nulos.
+		O método get() do Optional retorna o objeto contido dentro dele.
+		Problema: Se o Optional estiver vazio (ou seja, se não encontrar um usuário com o ID fornecido), get()
+		 lança uma exceção NoSuchElementException
+		 pode-se tratar esse erro usando ".orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));"
+		*/
+		Optional<User>op = repository.findById(id);
+		return op.get();
+		
+	}
+}
