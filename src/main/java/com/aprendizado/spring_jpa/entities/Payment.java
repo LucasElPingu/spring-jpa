f1a8077facdd49032075ad739f0dbd7cf18b6225
LucasElPingu <lucasfernando.soaresssilva@gmail.com>
2025-03-08 15:36:15 -0300

    Adição dos métodos total e subtotal

--- src/main/java/com/aprendizado/spring_jpa/entities/Payment.java
+++ src/main/java/com/aprendizado/spring_jpa/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moments;
 	
+	@JsonIgnore
 	@OneToOne
 	/*
 	 Diz ao JPA que a chave primária da entidade atual deve ser a mesma que a chave primária da entidade Order.
